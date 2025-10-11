8f556150882731887a6fdaa051980c869fa0f27b
Darshan Rajput <darshan.rajput123091@marwadiuniversity.ac.in>
2025-10-11 15:20:23 +0530

    Delete config directory
    detele the congif

--- config/firebase.d.ts
+++
@@ -1,9 +0,0 @@
-import type { Auth } from 'firebase/auth';
-import type { Firestore } from 'firebase/firestore';
-
-declare module '../config/firebase' {
-  export const auth: Auth;
-  export const db: Firestore;
-}
-
-export {};
